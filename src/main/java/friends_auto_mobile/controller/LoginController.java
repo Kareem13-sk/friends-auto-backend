94aac89520866ecc20ec6047aeeec2d2e2931e2b
Abdul Kareem <kareemaleem007@gmail.com>
2026-05-31 11:34:12 +0530

    Added test users endpoint-1

--- src/main/java/friends_auto_mobile/controller/LoginController.java
+++ src/main/java/friends_auto_mobile/controller/LoginController.java
@@ -2,7 +2,6 @@ package friends_auto_mobile.controller;
 
 import friends_auto_mobile.entity.User;
 import friends_auto_mobile.repository.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +14,8 @@ public class LoginController {
     private UserRepository repository;
 
     @PostMapping("/login")
-    public User login(@RequestBody User request) {
+    public User login(
+            @RequestBody User request) {
 
         return repository
                 .findByUsernameAndPassword(
@@ -32,5 +32,33 @@ public class LoginController {
     @GetMapping("/test-users")
     public Object testUsers() {
         return repository.findAll();
+    }
+
+    @GetMapping("/seed-users")
+    public String seedUsers() {
+
+        if (repository.count() > 0) {
+            return "Users already exist";
+        }
+
+        User admin = new User();
+        admin.setUsername("admin");
+        admin.setPassword("admin123");
+        admin.setRole("ADMIN");
+        repository.save(admin);
+
+        User naimulla = new User();
+        naimulla.setUsername("naimulla");
+        naimulla.setPassword("shop123");
+        naimulla.setRole("ADMIN");
+        repository.save(naimulla);
+
+        User staff = new User();
+        staff.setUsername("staff1");
+        staff.setPassword("staff123");
+        staff.setRole("STAFF");
+        repository.save(staff);
+
+        return "Users Added Successfully";
     }
 }
